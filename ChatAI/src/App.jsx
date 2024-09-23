e23db1757f231e10a84deb9ca50b8300ef2e06c2
Vikrant Yadav <vikrantmani45@gmail.com>
2024-09-23 07:23:40 +0530

    simple axios integration

--- ChatAI/src/App.jsx
+++ ChatAI/src/App.jsx
@@ -2,14 +2,29 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import axios from 'axios';
+
 
 function App() {
   const [count, setCount] = useState(0)
+
+  async function generateAnswer(){
+    console.log("loaidng");
+    const response=await axios({
+      link:"https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash-latest:generateContent?key=AIzaSyDERfqLZCOMtT_AOfq6ZPPNj5eJ0sAzdj8",
+      method: "post",
+        data: {
+          contents: [{ parts: [{ text: question }] }],
+        },
+      });
+    console.log(response);
+  }
 
   return (
     <>
       <div>
         <h1>ChatAI Application</h1>
+        <button onClick={generateAnswer}>Generate Answer</button>
       </div>
       
     </>
